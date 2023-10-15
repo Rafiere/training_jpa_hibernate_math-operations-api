dc9eef42a7e1459d4b3b0e200b27433aeb57a748
Rafiere <rafapgbr@gmail.com>
2023-10-14 23:18:54 -0300

    feat: add initial application files

---
+++ README.md
@@ -0,0 +1,10 @@
+# MATH OPERATIONS API
+
+Essa é uma API simples para a realização de alguns testes e apenas com fins educacionais. Não estou utilizando as melhores práticas nem as melhores bibliotecas nesse projeto.
+
+## TECNOLOGIAS TESTADAS
+
+- Spring Boot
+- Spring Web
+- Lombok
+- Hibernate
---
+++ src/main/java/com/piccodev/mathoperationsapi/MathOperationsApiApplication.java
@@ -0,0 +1,12 @@
+package com.piccodev.mathoperationsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MathOperationsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MathOperationsApiApplication.class, args);
+	}
+}
