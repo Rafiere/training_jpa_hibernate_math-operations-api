14ec616ab5ef105f51ebb47dcd9859c1af688bf8
Rafiere <rafapgbr@gmail.com>
2023-10-21 15:42:37 -0300

    feat: add @Transactional.

--- src/main/java/com/piccodev/mathoperationsapi/math/repository/OperationRepositoryImpl.java
+++ src/main/java/com/piccodev/mathoperationsapi/math/repository/OperationRepositoryImpl.java
@@ -4,15 +4,18 @@ import com.piccodev.mathoperationsapi.math.domain.Operation;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 @Repository
 public class OperationRepositoryImpl implements OperationRepository {
 
-    @PersistenceContext(name = "Operations-PU")
+    @PersistenceContext
     private EntityManager entityManager;
 
     @Override
+    @Transactional
     public void save(final Operation operation) {
+
         entityManager.persist(operation);
     }
 }
